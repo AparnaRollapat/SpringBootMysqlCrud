41b3093624e8d534b308c046fd3cf9190cf7988b
Deniz TÜRKMEN <50116324+denizturkmen@users.noreply.github.com>
2021-10-12 21:21:07 +0300

    Swagger

--- Readme.md
+++ Readme.md
@@ -4,3 +4,4 @@
 # mvn clean
 # mvn install
 # Database oluştur ve application properties belirt
+# http://localhost:8082/swagger-ui/index.html?
--- src/main/java/com/denizturkmen/crudexample/CrudExampleApplication.java
+++ src/main/java/com/denizturkmen/crudexample/CrudExampleApplication.java
@@ -1,11 +1,13 @@
 package com.denizturkmen.crudexample;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@OpenAPIDefinition(info = @Info(title = "Person API", version = "2.0", description = "Person Information"))
 public class CrudExampleApplication {
 
     public static void main(String[] args) {
